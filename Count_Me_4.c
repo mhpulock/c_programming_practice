283e5b17580ed92cc89edb3a9320fa70583ce7f4
mhpulock <ippolok24@gmail.com>
2025-04-27 16:40:43 +0600

    freequency string related problem solving

--- Count_Me_4.c
+++ Count_Me_4.c
@@ -4,9 +4,19 @@ int main()
 {
   char arr[10000];
   scanf("%s", &arr);
+  int freeq[26] = {0};
   for (int i = 0; i < strlen(arr) ; i++)
   {
-    arr[i] 
+    int indx = arr[i]-97;
+    freeq[indx]++;
+  }
+  for (int i = 0; i < 26; i++)
+  {
+    if (freeq[i] > 0)
+    {
+      printf("%c - %d\n", i+97, freeq[i]);
+    }
+    
   }
   
   return 0;
